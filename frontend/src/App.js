a43c8d83532de1e1c4b759b797e5af164e86949e
gazifaizan <mdgazifaizan@gmail.com>
2023-03-17 00:37:29 +0530

    new text

---
+++ frontend/src/App.js
@@ -0,0 +1,21 @@
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import SignupForm from './component/signup';
+import LoginForm from './component/login';
+import PostOffer from './component/offer';
+import Cart from './component/cart';
+function App() {
+  return (
+    <>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/signup" element={<SignupForm />} />
+          <Route path='/' element={<LoginForm />} />
+          <Route path='/admin' element={<PostOffer />} />
+          <Route path="/cart" element={<Cart />} />
+        </Routes>
+      </BrowserRouter>
+    </>
+  );
+}
+
+export default App;
